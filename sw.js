32e37fc676bace580bcbd516504db699b9af9bde
thebrxm <75737264+thebrxm@users.noreply.github.com>
2025-08-08 09:58:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE = 'contador-mci-v5'; // mude o sufixo!
+const CACHE = 'contador-mci-v6'; // mude o sufixo pra forçar atualização
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './favicon.png'
+  './ecues-192.png',
+  './ecues-512.png',
+  './apple-touch-icon.png'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
 });
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
